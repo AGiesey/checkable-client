054adfd783e31631b4c4c6ee5af824730b90978f
AGiesey <adamgiesey@gmail.com>
2019-03-23 08:16:25 -0700

    use correct redux devtools function

--- src/_redux/store.js
+++ src/_redux/store.js
@@ -2,7 +2,8 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import thunkMiddleware from 'redux-thunk';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ || compose;
+// TODO: Remove redux devtools in production env.
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default createStore(
   rootReducer,
   composeEnhancers(
